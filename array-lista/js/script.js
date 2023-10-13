807aaa7ad85d623e22082dc5b2936fb13809a410
Hycarus <mirkomarasco08@gmail.com>
2023-10-13 16:10:47 +0200

    added function for insert fruit in the fridge

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -15,6 +15,25 @@ const resultEl = document.querySelector('.alert');
 let msg;
 let found = false;
 let cocomero = 'cocomero';
+btn.addEventListener('click', function(){
+    let alertColor = 'alert-success';
+    resultEl.classList.remove('alert-danger');
+    resultEl.classList.add('d-none');
+    let valoreInserito = (inputEl.value);
+    if(!isNaN(valoreInserito)){
+        console.log('errore');
+        alertColor = 'alert-danger';
+        msg = 'Hai inserito un numero!';
+        console.log(valoreInserito);
+    } else{
+        msg = 'Frutto inserito!';
+        fridge.push(valoreInserito);
+        console.log(fridge);
+    }
+    resultEl.classList.add(alertColor);
+    resultEl.classList.remove('d-none');
+    resultEl.innerText = msg;
+})
 fridge.push('pesca');
 console.log(fridge);
 for(i = 0; i < fridge.length; i++){
