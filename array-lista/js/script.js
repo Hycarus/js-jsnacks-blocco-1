5ab6a01060616437634d37fd311ec96bca8cb4ba
Hycarus <mirkomarasco08@gmail.com>
2023-10-13 16:43:55 +0200

    added function for search fruit

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -13,10 +13,9 @@ const btn = document.querySelector('button');
 const inputEl = document.getElementById('data');
 const resultEl = document.querySelector('.alert');
 let msg;
-let found = false;
-let cocomero = 'cocomero';
 btn.addEventListener('click', function(){
     let alertColor = 'alert-success';
+    resultEl.classList.remove('alert-success');
     resultEl.classList.remove('alert-danger');
     resultEl.classList.add('d-none');
     let valoreInserito = (inputEl.value);
@@ -36,13 +35,38 @@ btn.addEventListener('click', function(){
 })
 fridge.push('pesca');
 console.log(fridge);
-for(i = 0; i < fridge.length; i++){
-    if(cocomero === fridge[i]){
-        found = true;
-    } 
-}
-if(found === true){
-    console.log("Trovato! Devo solo preparare il cocktail.");
-} else{
-    console.log("Oh no, devo uscire a comprare il cocomero!");
-}
+
+const btnSearch = document.querySelector('.button');
+const searchEl = document.getElementById('search');
+btnSearch.addEventListener('click', function(){
+    let alertColor = 'alert-success';
+    resultEl.classList.remove('alert-success');
+    resultEl.classList.remove('alert-danger');
+    resultEl.classList.add('d-none');
+    let valoreInserito = (searchEl.value);
+    let frutto = valoreInserito;
+    let found = false;
+    if(!isNaN(valoreInserito)){
+        console.log('errore');
+        alertColor = 'alert-danger';
+        msg = 'Hai inserito un numero!';
+        console.log(valoreInserito);
+    } else{
+        for(i = 0; i < fridge.length; i++){
+            if(frutto === fridge[i]){
+                found = true;
+            } 
+        }
+        if(found === true){
+            msg = 'Trovato! Devo solo preparare il cocktail.';
+            console.log("Trovato! Devo solo preparare il cocktail.");
+        } else{
+            msg = 'Oh no, devo uscire a comprare' + " " + frutto;
+            alertColor = 'alert-danger';
+            console.log("Oh no, devo uscire a comprare" + " " + frutto);
+        }
+    }
+    resultEl.classList.add(alertColor);
+    resultEl.classList.remove('d-none');
+    resultEl.innerText = msg;
+})
