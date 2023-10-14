6242a74702fac08ce12654802cdccc4bc7ace79a
Hycarus <mirkomarasco08@gmail.com>
2023-10-14 14:09:02 +0200

    created scaffholding bonus

---
+++ array_bonus/js/script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('button');
+const inputEl = document.getElementById('data');
+const resultEl = document.querySelector('.alert');
+const resultElTwo = document.querySelector('.alert-2');
+const btnSearch = document.querySelector('.button');
+const searchEl = document.getElementById('search');
+let msg = ' ';
+let msg2 = ' ';
